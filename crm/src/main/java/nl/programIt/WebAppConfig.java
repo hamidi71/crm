18c738c7c6e2f7c6644961bdc3ed6658d6e58a8b
hamidi71 <chiahna71@gmail.com>
2016-09-07 06:58:19 +0200

    thymeleaf en thymeleaf configuratie

--- crm/src/main/java/nl/programIt/WebAppConfig.java
+++ crm/src/main/java/nl/programIt/WebAppConfig.java
@@ -2,6 +2,7 @@ package nl.programIt;
 
 import java.util.Locale;
 
+import org.hibernate.property.Getter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -14,6 +15,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.spring4.view.ThymeleafViewResolver;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
 
 //zie mvc-context
 @Configuration //normaal config
@@ -27,22 +31,39 @@ public class WebAppConfig extends WebMvcConfigurerAdapter{
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/public/");
 	}
-	
-
-	@Bean
-	public InternalResourceViewResolver resolverView(){
-		InternalResourceViewResolver resolver= new InternalResourceViewResolver();
-		resolver.setSuffix(".jsp");
-		resolver.setPrefix("/WEB-INF/views/");
+//config Theamleaf
+	//zoekt thymleafplaats
+	@Bean(name="templateResolver")
+	public ServletContextTemplateResolver templateResolver(){
+		ServletContextTemplateResolver resolver=new ServletContextTemplateResolver();
+		resolver.setPrefix("/WEB-INF/templates/");
+		resolver.setSuffix(".html");
+		resolver.setTemplateMode("HTML5");
 		return resolver;
+	} 
+	//config en ralatie tussen spring mvc en thymeleaf
+	@Bean(name="templateEngine")
+	public SpringTemplateEngine templateEngine(){
+		SpringTemplateEngine engine=new SpringTemplateEngine();
+		engine.setTemplateResolver(templateResolver());		
+		return engine;	
 	}
+	//resolver analyset werkt aleen met thymeleaf
+	@Bean(name="viewResolver")	
+	 public ThymeleafViewResolver getViewResolver(){
+    	ThymeleafViewResolver resolver = new ThymeleafViewResolver(); 
+    	resolver.setTemplateEngine(templateEngine());
+	return resolver;
+    }	
 	
+//Hier onde zijn de beans van i18n(internationalisation) voor de taals
 	@Bean
 	public ResourceBundleMessageSource messageSource(){
 		ResourceBundleMessageSource messageS =new ResourceBundleMessageSource();
 		messageS.setBasename("messages");//name zonder code _en.propreties		
 		return messageS;
 	}
+	
 	
 	@Bean //Taal par default
 	public LocaleResolver localeResolver(){
@@ -60,7 +81,6 @@ public class WebAppConfig extends WebMvcConfigurerAdapter{
 		changeInterceptor.setParamName("lang");		
 		return changeInterceptor;
 	}
-
 	
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
--- crm/src/main/webapp/WEB-INF/views/index.jsp
+++
@@ -1,3 +0,0 @@
-<%@include file="template/header.jsp" %>
-<div class="container"></div>
-<%@include file="template/footer.jsp" %>
